cb2b758eefa3eabd43f0a6c5b1c25537b4b4a05a
Mika Laitio <lamikr@gmail.com>
2024-01-26 02:10:31 -0800

    env_rocm_template.sh cleanup
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- binfo/env/env_rocm_template.sh
+++ binfo/env/env_rocm_template.sh
@@ -1,13 +1,9 @@
-export LLVM_HOME=${ROCM_HOME}/llvm
 export HIP_HOME=${ROCM_HOME}
+export LLVM_HOME=${ROCM_HOME}/llvm
 
-export LD_LIBRARY_PATH=${HIP_HOME}/lib:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${LLVM_HOME}/lib:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=${ROCM_HOME}/lib64:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=${ROCM_HOME}/lib:${LD_LIBRARY_PATH}
-#export LD_LIBRARY_PATH=${ROCM_HOME}/python38/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${LLVM_HOME}/lib:${LD_LIBRARY_PATH}
 
-export PATH=${HIP_HOME}/bin:${PATH}
 export PATH=${LLVM_HOME}/bin:${PATH}
 export PATH=${ROCM_HOME}/bin:${PATH}
-#export PATH=${ROCM_HOME}/python38/bin:${PATH}
