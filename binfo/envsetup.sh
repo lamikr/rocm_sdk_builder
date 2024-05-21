18109928b08c537cec26db0b9a36428e81e0c8cf
Mika Laitio <lamikr@gmail.com>
2024-05-21 13:31:17 -0700

    envsetup added more gpu's to selection list
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- binfo/envsetup.sh
+++ binfo/envsetup.sh
@@ -60,7 +60,7 @@ export CPACK_RPM_PACKAGE_RELEASE=01
 export python=python
 
 func_build_cfg_user() {
-    ./build/checkbox.sh --message="Select ROCM SDK build target GPUs. Space to select, Enter to finish save, ESC to cancel." --options="gfx1010|gfx1030|gfx1035" --multiple
+    ./build/checkbox.sh --message="Select ROCM SDK build target GPUs. Space to select, Enter to finish save, ESC to cancel." --options="gfx906|gfx90a|gfx940|gfx1010|gfx1011|gfx1012|gfx1030|gfx1031|gfx1035|gfx1100|gfx1101|gfx1102|gfx1150|gfx1151" --multiple
 }
 
 USER_CFG_FNAME='build_cfg.user'
