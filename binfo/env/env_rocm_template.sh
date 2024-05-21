28043fbbbcd7d1c043872ad48cc7c1af862fe8a9
Mika Laitio <lamikr@gmail.com>
2024-05-21 13:34:04 -0700

    added PYTHONPATH to env_rocm.sh template
    - without this the MIGRAPHX does not work
    
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- binfo/env/env_rocm_template.sh
+++ binfo/env/env_rocm_template.sh
@@ -5,6 +5,10 @@ export MAGMA_HOME==${ROCM_HOME}
 
 export LD_LIBRARY_PATH=${ROCM_HOME}/lib64:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=${ROCM_HOME}/lib:${LD_LIBRARY_PATH}
+
 export DEVICE_LIB_PATH=${ROCM_HOME}/amdgcn/bitcode
+
+# pythonpath is required at least by AMDMIGraphX pytorch module
+export PYTHONPATH=${ROCM_HOME}/lib64:${ROCM_HOME}/lib:$PYTHONPATH
 
 export PATH=${ROCM_HOME}/bin:${PATH}
