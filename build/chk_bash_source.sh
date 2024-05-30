28f05e82ef425c9553aef5da10e826798cc3e299
Jasson <jasson.cordones@hotmail.com>
2024-05-30 13:11:44 -0400

    Revert chk_bash_source

--- build/chk_bash_source.sh
+++ build/chk_bash_source.sh
@@ -3,9 +3,10 @@
 # This script needs to be sourced
 
 check_bash_sourced() {
-    if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-        echo "Script execution failed, script must be called as sourced!"
-        echo "source ${BASH_SOURCE[0]}"
-        exit 1
-    fi
+	if [ "${0:0:4}" != "bash" ] && [ "${0:0:4}" != "-bas" ]; then
+		echo "Script execution failed. The script must be sourced."
+		echo "Please use the following command to source the script:"
+		echo "source $0"
+		exit 1
+	fi
 }
