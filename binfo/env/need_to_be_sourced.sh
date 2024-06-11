066db665b07774be58802dddb3bb607bc7eef508
Daniele <57776841+daniandtheweb@users.noreply.github.com>
2024-06-11 17:36:08 +0000

    Fix crash on any shell other than bash

--- binfo/env/need_to_be_sourced.sh
+++ binfo/env/need_to_be_sourced.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script needs to be sourced
-if [ "${0:0:4}" != "bash" ] && [ "${0:0:4}" != "-bas" ]; then
+if [[ "$0" != *"sh"*]]; then
     echo "Script execution failed. Please use the following command to source the script:"
     echo "    source $0"
     echo ""
