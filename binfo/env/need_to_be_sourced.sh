a080843a2f567fec8d1e3ac0d3377822c35b99cf
Daniele <57776841+daniandtheweb@users.noreply.github.com>
2024-06-14 23:24:46 +0000

    Change source check

--- binfo/env/need_to_be_sourced.sh
+++ binfo/env/need_to_be_sourced.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script needs to be sourced
-if [ "${0:0:4}" != "bash" ] && [ "${0:0:4}" != "-bas" ]; then
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     echo "Script execution failed. Please use the following command to source the script:"
     echo "    source $0"
     echo ""
