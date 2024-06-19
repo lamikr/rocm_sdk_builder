bd028137565c837b2e05c7278d43716e1cc63878
Mika Laitio <lamikr@gmail.com>
2024-06-19 15:23:54 -0700

    removei binfo install references to hardcoded paths
    - use environment variables instead to point to the
      correct folder
    - fixes https://github.com/lamikr/rocm_sdk_builder/issues/77
    
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- binfo/env/env_rocm_template.sh
+++ binfo/env/env_rocm_template.sh
@@ -13,7 +13,7 @@ export DEVICE_LIB_PATH=${ROCM_HOME}/amdgcn/bitcode
 # the /etc/OpenCL/vendors/ with this variable
 # note that the path must end with the slash "/" here, otherwise the
 # implementation fails to open files it has scanned from directory.
-export OCL_ICD_VENDORS=/opt/rocm_sdk_611/etc/OpenCL/vendors/
+export OCL_ICD_VENDORS=${ROCM_HOME}/etc/OpenCL/vendors/
 
 # pythonpath is required at least by AMDMIGraphX pytorch module
 export PYTHONPATH=${ROCM_HOME}/lib64:${ROCM_HOME}/lib:$PYTHONPATH
