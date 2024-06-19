153e2421eada185b4527bf34ee1dae14eafcd322
Mika Laitio <lamikr@gmail.com>
2024-06-19 16:04:19 -0700

    remove binfo comment references to 611 to xyz
    - helps detecting places which needs to be changed
      when porting code to new rocm version
    
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- binfo/build_version.sh
+++ binfo/build_version.sh
@@ -8,18 +8,18 @@
 # Three first numbers are used for example to create a string that
 # some applications like pytorch uses in #ifdefs
 #
-# In addition content of /opt/rocm_sdk_611/.info/version is bases on to these numbers
+# In addition content of /opt/rocm_sdk_xyz/.info/version is bases on to these numbers
 export ROCM_MAJOR_VERSION=6
 export ROCM_MINOR_VERSION=1
 export ROCM_PATCH_VERSION=2
 export ROCM_SDK_RELEASE_VERSION=1
 
 # Set BABS version information
-# Stored in /opt/rocm_sdk_611/.info/rocm_sdk_builder
+# Stored in /opt/rocm_sdk_xyz/.info/rocm_sdk_builder
 export BABS_VERSION=2024_06_12_01
 
 # Get git hash used to build the system
-# Stored in /opt/rocm_sdk_611/.info/rocm_sdk_builder
+# Stored in /opt/rocm_sdk_xyz/.info/rocm_sdk_builder
 export ROCM_SDK_BUILDER_SRC_REV=$(git rev-parse --short=8 HEAD)
 
 export ROCM_SDK_VERSION_INFO=rocm-${ROCM_MAJOR_VERSION}.${ROCM_MINOR_VERSION}.${ROCM_PATCH_VERSION}
