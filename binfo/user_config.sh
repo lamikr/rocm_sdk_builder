457ec98164ff418d23262bdd22898b6e6cf3de01
Mika Laitio <lamikr@gmail.com>
2024-08-18 14:53:02 -0700

    add gfx1032 to selection list
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- binfo/user_config.sh
+++ binfo/user_config.sh
@@ -3,7 +3,7 @@
 # Function to select ROCM SDK build target GPUs
 func_build_cfg_user() {
     local message="Select ROCM SDK build target GPUs. Space to select, Enter to finish save, ESC to cancel."
-    local options="gfx906|gfx90a|gfx940|gfx1010|gfx1011|gfx1012|gfx1030|gfx1031|gfx1035|gfx1036|gfx1100|gfx1101|gfx1102|gfx1103|gfx1150|gfx1151"
+    local options="gfx906|gfx90a|gfx940|gfx1010|gfx1011|gfx1012|gfx1030|gfx1031|gfx1032|gfx1035|gfx1036|gfx1100|gfx1101|gfx1102|gfx1103|gfx1150|gfx1151"
     local script_path="./build/checkbox.sh"
 
     # Check if the script exists and is executable
