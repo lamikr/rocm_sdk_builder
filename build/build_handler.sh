d1acb40def7bfd0e23be10273dcd5a12380e4d0e
Mika Laitio <lamikr@gmail.com>
2024-08-21 12:57:15 -0700

    babs.sh command improvements
    - add support for blist files which can define additional
      list of binfo-application files to build
    - following blist commands works:
       - ./babs.sh -i binfo/extra/ai_tools.blist
       - ./babs.sh -f binfo/extra/ai_tools.blist
       - ./babs.sh -co binfo/extra/ai_tools.blist
       - ./babs.sh -ca binfo/extra/ai_tools.blist
       - ./babs.sh -b binfo/extra/ai_tools.blist
    - improve checkout (co) and fetch commands to
      clone a repository if it does not exist yet
    - improve checkout command to fetch repository automatically
      if it can not find the requested revision
    - fetch submodules automatically during git fetch
    - removed the submodule fetch command that is not needed
      anymore
    
    Signed-off-by: Mika Laitio <lamikr@gmail.com>
    Signed-off-by: Jasson <jasson.cordones@hotmail.com>

--- build/build_handler.sh
+++ build/build_handler.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source build/system_utils.sh
 source build/config.sh
+source build/build_func.sh
 
 func_babs_handle_build() {
     func_env_variables_print #From config.sh
@@ -9,11 +10,14 @@ func_babs_handle_build() {
     local ret_val=$?
     if [[ $ret_val -eq 0 ]]; then
         if [[ -n "$1" ]]; then
-            echo "func_build param: $1"
             source ./build/build_func.sh
-            func_init_and_build_single_binfo $1
+            if [[ "$1" = *.binfo ]] ; then
+                func_init_and_build_single_binfo $1
+            fi
+            if [[ "$1" = *.blist ]] ; then
+                func_init_and_build_blist $1
+            fi
         else
-            echo "func_build no params"
             source ./build/build_core.sh
         fi
         res=$?
@@ -34,5 +38,95 @@ func_babs_handle_build() {
             echo -e "Failed to build ROCM_SDK_BUILDER"
             echo ""
         fi
+    fi
+}
+
+func_babs_handle_checkout() {
+    if [[ -n "$1" ]]; then
+        echo "func_babs_handle_checkout param: $1"
+        if [[ "$1" = *.binfo ]] ; then
+            func_envsetup_init
+            func_babs_checkout_by_binfo $1
+        fi
+        if [[ "$1" = *.blist ]] ; then
+            func_envsetup_init
+            func_babs_checkout_by_blist $1
+        fi
+    else
+        echo "func_babs_handle_checkout no params"
+        func_repolist_checkout_default_versions
+    fi
+}
+
+func_babs_handle_apply_patches() {
+    if [[ -n "$1" ]]; then
+        echo "func_babs_handle_patch_apply param: $1"
+        if [[ "$1" = *.binfo ]] ; then
+            func_envsetup_init
+            func_babs_apply_patches_by_binfo ${ARG__USER_CMD_PARAM1} #From build/binfo_operations.sh
+        fi
+        if [[ "$1" = *.blist ]] ; then
+            func_envsetup_init
+            func_babs_apply_patches_by_blist ${ARG__USER_CMD_PARAM1} #From build/binfo_operations.sh
+        fi
+    else
+        echo "func_babs_handle_patch_apply no params"
+        func_repolist_apply_patches build/binfo_operations.sh
+    fi
+}
+
+func_babs_handle_checkout_and_apply_patches() {
+    if [[ -n "$1" ]]; then
+        echo "func_babs_handle_checkout_and_apply_patches param: $1"
+        if [[ "$1" = *.binfo ]] ; then
+            func_envsetup_init
+            func_babs_checkout_by_binfo ${ARG__USER_CMD_PARAM1}
+            func_babs_apply_patches_by_binfo ${ARG__USER_CMD_PARAM1}
+        fi
+        if [[ "$1" = *.blist ]] ; then
+            func_envsetup_init
+            func_babs_checkout_by_blist ${ARG__USER_CMD_PARAM1}
+            func_babs_apply_patches_by_blist ${ARG__USER_CMD_PARAM1}
+        fi
+    else
+        func_repolist_checkout_default_versions
+        func_repolist_apply_patches build/binfo_operations.sh
+    fi
+}
+
+func_babs_handle_repository_fetch() {
+    if [[ -n "$1" ]]; then
+        echo "func_babs_handle_fetch param: $1"
+        if [[ "$1" = *.binfo ]] ; then
+            func_envsetup_init
+            func_babs_init_and_fetch_by_binfo ${ARG__USER_CMD_PARAM1}
+        fi
+        if [[ "$1" = *.blist ]] ; then
+            func_envsetup_init
+            func_babs_init_and_fetch_by_blist ${ARG__USER_CMD_PARAM1}
+        fi
+    else
+        func_repolist_fetch_core_repositories
+    fi
+}
+
+# todo: Check whether these functions used here can be replaced with the ones
+# on func_babs_handle_repository_fetch
+# because the functionality in these functions is now most likely exactly similar
+# than in the fetch functions. Fetch functions has been updated to
+# handle the checkout, apply patches and submodules also if the repositories does not exist.
+func_babs_handle_repository_init() {
+    if [[ -n "$1" ]]; then
+        echo "func_babs_handle_init param: $1"
+        if [[ "$1" = *.binfo ]] ; then
+            func_envsetup_init
+            func_upstream_remote_repo_add_by_binfo ${ARG__USER_CMD_PARAM1}
+        fi
+        if [[ "$1" = *.blist ]] ; then
+            func_envsetup_init
+            func_upstream_remote_repo_add_by_blist ${ARG__USER_CMD_PARAM1}
+        fi
+    else
+        func_repolist_upstream_remote_repo_add #From build/repo_management.sh
     fi
 }
