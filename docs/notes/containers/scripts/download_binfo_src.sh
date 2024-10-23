97e947264b5e5b1edd66bdd10736e791b059feb6
Mika Laitio <lamikr@gmail.com>
2024-10-23 09:50:45 -0700

    updated test scripts for docker build
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

---
+++ docs/notes/containers/scripts/download_binfo_src.sh
@@ -0,0 +1,5 @@
+./babs.sh -i
+./babs.sh -i binfo/extra/ai_tools.blist
+./babs.sh -i binfo/extra/amd_devel_tools.blist
+./babs.sh -i binfo/extra/amd_media_tools.blist
+./babs.sh -i binfo/extra/google_tools.blist
