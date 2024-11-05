75a36f887c2e3625205a5235d4b9e38483db52b3
Mika Laitio <lamikr@pilppa.org>
2024-11-05 00:10:32 -0800

    install pytorch flash attention examples
    Signed-off-by: Mika Laitio <lamikr@pilppa.org>

---
+++ docs/examples/pytorch/flash_attention/flash_attention_enable_disable_test.sh
@@ -0,0 +1,11 @@
+# pytorch example rocm sdk launcher
+# if test fails on AMD GPU, enable AMD_LOG_LEVEL and HIP_VISIBLE_DEVICES=0 variables
+# to get traces to find the failing code part
+if [ -z $ROCM_HOME ]; then
+    echo "Error, make sure that you have executed"
+    echo "    source  /opt/rocm_sdk_612/bin/env_rocm.sh"
+    echo "before running this script"
+    exit 1
+fi
+#AMD_LOG_LEVEL=1 HIP_VISIBLE_DEVICES=0 HIP_LAUNCH_BLOCKING=1 python pytorch_gpu_simple_test.py
+python flash_attention_enable_disable_test.py
