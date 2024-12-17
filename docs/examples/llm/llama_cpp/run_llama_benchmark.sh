e42327820696118e8aebf35af47ecbefeeae1bf6
Mika Laitio <lamikr@pilppa.org>
2024-12-17 15:37:46 -0800

    llama.cpp example script improvements
    - add script to check that model file
      has been downloaded
    - add script to check that llama is on path
    - build llama earlier
    - pip install transformers version 4.45.0
    
    Signed-off-by: Mika Laitio <lamikr@pilppa.org>

--- docs/examples/llm/llama_cpp/run_llama_benchmark.sh
+++ docs/examples/llm/llama_cpp/run_llama_benchmark.sh
@@ -7,5 +7,27 @@ if [ -z $ROCM_HOME ]; then
     echo "before running this script"
     exit 1
 fi
+MODEL_FNAME=/opt/rocm_sdk_models/microsoft/Phi-3-mini-4k-instruct-q4.gguf
+MODEL_DOWNLOAD_URL=https://huggingface.co/microsoft/Phi-3-mini-4k-instruct-gguf/tree/main
 #CUDA_VISIBLE_DEVICES=2
-llama-cli -ngl 0 -m /opt/rocm_sdk_models/microsoft/Phi-3-mini-4k-instruct-q4.gguf -n 10 -f <(printf 'banana %0.s' {1..50}) -v 2>&1 | grep timings
+if [ ! -f $MODEL_FNAME ]; then
+    echo "Could not find model file:"
+    echo "    $MODEL_FNAME"
+    echo "Try to download and install it from:"
+    echo "    $MODEL_DOWNLOAD_URL"
+fi
+if [ -f $MODEL_FNAME ]; then
+    PATH_TO_EXEC=$(which llama-cli)
+    if [ -x "$PATH_TO_EXEC" ] ; then
+	echo ""
+        echo "llama-cli -ngl 0 -m $MODEL_FNAME -n 10 -f <(printf 'banana %0.s' {1..50})"
+	echo ""
+        llama-cli -ngl 0 -m $MODEL_FNAME -n 10 -f <(printf 'banana %0.s' {1..50}) -v 2>&1 | grep time
+    else
+	echo ""
+	echo "Could not find llama-cli"
+	echo "Have you build it with command?"
+        echo "    ./babs.sh -b binfo/extra/ai_tools.blist"
+	echo ""
+    fi
+fi
